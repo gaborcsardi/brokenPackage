d3e3131c427e5bab504960420b46f9cb6c615c78
Malcolm Morgan <mem48@users.noreply.github.com>
2020-12-10 18:55:22 +0000

    add other OS support

--- R/hello.R
+++ R/hello.R
@@ -3,6 +3,11 @@
 #' @export
 
 sleep_test <- function(wait){
-  r <- system("ping -n 5 127.0.0.1", intern = wait, wait = wait)
+
+  if(checkmate::test_os("windows")){
+    r <- system("ping -n 10 127.0.0.1", intern = wait, wait = wait)
+  } else {
+    r <- system("sleep 10", intern = wait, wait = wait)
+  }
   return(TRUE)
 }
